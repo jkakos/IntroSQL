f6c4095497a630aaabdd9de5289e8a789f245b76
James Kakos <jameskakos491@gmail.com>
2025-07-18 12:55:10 -0700

    Fixed inconsistent spacing from using tabs

--- 07_unions.sql
+++ 07_unions.sql
@@ -1,35 +1,35 @@
 -- UNION
 -- By default, UNION is UNION DISTINCT
 SELECT first_name
-	, last_name
+    , last_name
 FROM parks_and_recreation.employee_demographics
 UNION ALL
 SELECT first_name
-	, last_name
+    , last_name
 FROM parks_and_recreation.employee_salary;
 
 SELECT first_name
-	, last_name
+    , last_name
     , 'old_male' AS Label
 FROM parks_and_recreation.employee_demographics
 WHERE (
-	age > 40
+    age > 40
     AND gender = 'male'
 )
 UNION
 SELECT first_name
-	, last_name
+    , last_name
     , 'old_female' AS Label
 FROM parks_and_recreation.employee_demographics
 WHERE (
-	age > 40
+    age > 40
     AND gender = 'female'
 )
 UNION
 SELECT first_name
-	, last_name
+    , last_name
     , 'highly_paid' AS Label
 FROM parks_and_recreation.employee_salary
 WHERE salary > 70000
 ORDER BY first_name
-	, last_name;
+    , last_name;
