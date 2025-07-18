f199e7aae18c0b050d8d8c313163a004a6aabbdd
James Kakos <jameskakos491@gmail.com>
2025-07-18 12:56:16 -0700

    Fixed inconsistent spacing from using tabs

--- 09_case_statements.sql
+++ 09_case_statements.sql
@@ -15,23 +15,23 @@ FROM parks_and_recreation.employee_demographics;
 -- >= 50000 -> 7%
 -- Finance -> 10% bonus
 WITH temp AS (
-	SELECT *
-		, CASE
-			WHEN salary < 50000
-				THEN salary * 1.05
-			WHEN salary >= 50000
-				THEN salary * 1.07
-		  END AS 'new_salary'
-		, CASE
-			WHEN department_name = 'Finance'
-				THEN salary * 0.1
-				ELSE 0
-		  END AS 'bonus'
-	FROM parks_and_recreation.employee_salary AS sal
-	JOIN parks_and_recreation.parks_departments AS depts
-		ON sal.dept_id = depts.department_id
+    SELECT *
+        , CASE
+            WHEN salary < 50000
+                THEN salary * 1.05
+            WHEN salary >= 50000
+                THEN salary * 1.07
+          END AS 'new_salary'
+        , CASE
+            WHEN department_name = 'Finance'
+                THEN salary * 0.1
+                ELSE 0
+          END AS 'bonus'
+    FROM parks_and_recreation.employee_salary AS sal
+    JOIN parks_and_recreation.parks_departments AS depts
+        ON sal.dept_id = depts.department_id
 )
 SELECT *
-	, (new_salary - salary) + bonus AS 'total_pay_increase'
+    , (new_salary - salary) + bonus AS 'total_pay_increase'
 FROM temp
 ORDER BY total_pay_increase DESC;
