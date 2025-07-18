366ce177d231661e0eba15b24db539a9651fc480
James Kakos <jameskakos491@gmail.com>
2025-07-18 12:54:14 -0700

    Fixed inconsistent spacing from using tabs

--- 05_limit_and_alias.sql
+++ 05_limit_and_alias.sql
@@ -13,7 +13,7 @@ LIMIT 4, 2;
 
 -- Rename columns using an alias
 SELECT gender
-	, AVG(age) AS avg_age
+    , AVG(age) AS avg_age
 FROM parks_and_recreation.employee_demographics
 GROUP BY gender
 HAVING avg_age > 40;
